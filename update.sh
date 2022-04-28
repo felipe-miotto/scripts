fed2e473e9e850e2c902d7fdcbe21c97ba70f934
Felipe Miotto <103403005+felipe-miotto@users.noreply.github.com>
2022-04-28 13:27:41 -0300

    spaces

--- update.sh
+++ update.sh
@@ -1,15 +1,20 @@
 #!/opt/homebrew/bin/zsh
 
-echo 'Apps to update'
+echo 'Apps to update:'
 brew outdated
 brew outdated --cask
 mas outdated
 
+echo ' '
 echo 'Updating Applications...'
     brew upgrade
     brew update # Updating home-brew apps & formulas
     brew upgrade --cask # Updating apps installed as casks
     mas upgrade # Updating Mac App Store apps
+
+echo ' '
 echo 'Cleaning caches & directories...'
     brew cleanup -s # Clearing home-brew cache
+
+echo ' '
 echo 'Updating Complete!'
