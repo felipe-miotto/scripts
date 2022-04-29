e5afe88ca29618000b0261f1f0e873cc0a0615a9
Felipe Miotto <160151821@aluno.unb.br>
2022-04-29 18:40:10 -0300

    script for updating conda and mamba

--- update.sh
+++ update.sh
@@ -11,6 +11,8 @@ echo 'Updating Applications...'
     brew update # Updating home-brew apps & formulas
     brew upgrade --cask # Updating apps installed as casks
     mas upgrade # Updating Mac App Store apps
+    conda update conda
+    conda update mamba
 
 echo ' '
 echo 'Cleaning caches & directories...'
---
+++ updateconda.sh
@@ -0,0 +1,11 @@
+#!/opt/homebrew/bin/zsh
+
+echo 'Updating conda & mamba...'
+echo ' '
+
+conda update conda
+conda update mamba
+
+echo ' '
+echo 'Updating complete!'
+echo ' '
