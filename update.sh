4dbf8659818519ad6e86a8cc140981d8290ed5fc
Felipe Miotto <160151821@aluno.unb.br>
2022-05-01 02:38:40 -0300

    conda separado no updateconda

--- update.sh
+++ update.sh
@@ -11,8 +11,6 @@ echo 'Updating Applications...'
     brew update # Updating home-brew apps & formulas
     brew upgrade --cask # Updating apps installed as casks
     mas upgrade # Updating Mac App Store apps
-    conda update conda
-    conda update mamba
 
 echo ' '
 echo 'Cleaning caches & directories...'
